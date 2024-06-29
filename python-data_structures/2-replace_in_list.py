740207f7226c9c90c795df00a3851b5eb2c411e3
Phinatech <c.ugbo@alustudent.com>
2024-06-29 07:35:35 -0700

    pushing

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idk < 0:
+        return (my_list)
+    if idk > len(my_list) - 1:
+        return (my_list)
+    my_list[idx] = element
+    return(my_list)
