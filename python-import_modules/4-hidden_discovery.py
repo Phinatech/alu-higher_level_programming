5e458861d59f0d672d9552862b2830e1e126a011
Phinatech <c.ugbo@alustudent.com>
2024-06-29 04:59:01 -0700

    pushing

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    allfunc = dir()
-    for i in range(0, len(allfunc)):
-        if allfunc[1][:2] != "__":
-            print("{:s}".format(allfunc[i]))
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
