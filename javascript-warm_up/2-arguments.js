eb00399760e1f3dd456a3de003489478021117b8
Phinatech <c.ugbo@alustudent.com>
2024-09-26 09:32:11 -0700

    pushing

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const argv = process.argv.slice(2);
 const message = argv.length > 0 ? (argv.length === 1 ? 'Argument found' : 'Arguments found') : 'No argument';
-console.log(message);	
+console.log(message);
+
