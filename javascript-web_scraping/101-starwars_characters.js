694ce8f7d9fd3af5350a5d5bc23df5e8051bfe85
Phinatech <c.ugbo@alustudent.com>
2024-10-04 11:49:45 -0700

    pushing

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
-request(url, (err, res, body) => {
+request(url, function (err, res, body) {
   if (!err) {
     const characters = JSON.parse(body).characters;
-    characters.forEach(character => {
-      request(character, (err, res, body) => {
-        if (!err) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (err, res, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
